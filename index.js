155a2d56c43bd2ef62a7364ccc145ecb3540810b
JBlosil <150398479+JBlosil@users.noreply.github.com>
2023-12-04 14:14:02 -0700

    push

--- index.js
+++ index.js
@@ -39,65 +39,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/findRecord", (req, res) => {
-    res.render("findRecord", {});
-});
-
-app.get("/chooseBand/:bandName", (req, res) => {
-    knex.select("band_name", "lead_singer").from('bands').where("band_name", req.params.bandName).then(bands => {
-        res.render("displayBand", {mybands: bands});
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({err});
-    });
-});
-
-app.get("/editBand", (req, res) => {
-    knex.select("band_id", "band_name", "lead_singer").from("bands").where("band_name", req.query.bandName.toUpperCase()).then(bands => {
-        res.render("editBand", {mybands: bands});
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({err});
-    });    
-});
-
-
-app.post("/editBand", (req, res) => {
-    knex("bands").where("band_id", parseInt(req.body.bandID)).update({
-        band_name: req.body.bandName,
-        lead_singer: req.body.singer
-    }).then(mybands => {
-        res.redirect("/");
-    });    
-});
-
-app.get("/addBand", (req, res) => {
-    res.render("addBand");
-});
-
-app.post("/addBand", (req, res) => {
-    knex("bands").insert({band_name: req.body.band_name, lead_singer: req.body.lead_singer}).then(mybands => {
-        res.redirect("/");
-    })
-});
-
-app.post("/deleteBand/:id", (req, res) => {
-    knex("bands").where("band_id", req.params.id).del().then(mybands => {
-        res.redirect("/");
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({err});
-    })
-});
-
-app.post("/deleteAllBands", (req, res) => {
-    knex("bands").del().then(mybands => {
-        res.redirect("/");
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({err});
-    })
-});
 
 app.listen(port, () => console.log("Express App has started and server is listening!"));            
         
